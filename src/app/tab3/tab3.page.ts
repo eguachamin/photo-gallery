0c64e3c3a4f133c099b4db206aa3aa239666f723
eguachamin <evelyn.guachamin@epn.edu.ec>
2025-04-29 15:14:46 -0500

    Funcionalidad de Galeria Tab3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PhotoService } from "../services/photo.service";
 
 @Component({
   selector: 'app-tab3',
@@ -8,6 +9,13 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(public photoService:PhotoService) {}
+    addPhotoToGallery(){
+      this.photoService.addNewToGallery();
+      
+    }
+    async ngOnInit() {
+      await this.photoService.loadSaved();
+    }
 
 }
